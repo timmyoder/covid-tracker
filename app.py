5b40621dee1197e38c99ebd88c240d4d1e77a9d6
Tim Yoder <tim.a.yoder@gmail.com>
2020-11-17 18:26:35 -0800

    assign port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
+import os
 
 import matplotlib
 matplotlib.use('Agg')
@@ -52,4 +53,5 @@ def somerset():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
